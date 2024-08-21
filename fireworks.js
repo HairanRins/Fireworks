ff81a04c561bed0738fe45d7c6ec69c787719fba
H. Rino <159486391+HairanRins@users.noreply.github.com>
2024-08-21 13:46:50 +0300

    Add files via upload

---
+++ fireworks.js
@@ -0,0 +1,36 @@
+let t = 0;
+let c = document.querySelector("canvas");
+let $ = c.getContext('2d');
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+$.fillStyle = 'hsla(0,0%,0%,1)';
+
+window.addEventListener('resize', function () {
+    c.width  = this.window.innerWidth;
+    c.height = window.innerHeight;
+}, false);
+
+function draw() {
+    $.globalCompositeOperation = 'source-over';
+    $.fillStyle = 'hsla(0,0%,0%,.1)';
+    $.fillRect(0,0, c.width, c.height);
+    let foo,i,j,r;
+    foo = Math.sin(t)*2* Math.PI;
+    for (let i = 0; i < 400; i++) {
+        r = 400 * Math.sin(i*foo);
+        $.globalCompositeOperation = "";
+        $.fillStyle = 'hsla('+i+12+',100%,60%,1)';
+        $.beginPath();
+        $.arc(Math.sin(i)*r + (c.width/2), Math.cos(i)*r + (c.width/2), 1.5, 0, Math.PI*2);
+        $.fill();
+    }
+    t += 0.000005;
+    return t %= 2*Math.PI;
+};
+
+function run() {
+    window.requestAnimationFrame(run);
+    draw();
+};
+run();
+
